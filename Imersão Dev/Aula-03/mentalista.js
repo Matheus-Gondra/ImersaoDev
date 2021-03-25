fbb6b89b7d0ab2550010d7f03bd0b3970673aa6d
Matheus-Gondra <matheusgondra951@gmail.com>
2021-03-25 20:21:38 -0300

    Finalização do "mentalista" da aula 03

--- Imersão Dev/Aula-03/mentalista.js
+++ Imersão Dev/Aula-03/mentalista.js
@@ -1,2 +1,37 @@
-var resposta = document.querySelector("#resposta-input");
+
 var botao = document.querySelector(".botao-resposta");
+var recarregar = document.querySelector("#recarregar");
+var mensagem = document.querySelector("#mensagem");
+var numeroSorteado = parseInt((Math.random()*(30-10)+10));
+var tentativas = 3;
+
+function escrever(texto){
+    return mensagem.innerHTML = texto;
+}
+
+escrever("Escolhar um número entre 10 e 30");
+
+botao.addEventListener("click", function(){
+   
+    
+         var resposta = document.querySelector("#resposta-input").value;
+    
+        if(resposta == numeroSorteado){
+            escrever("Você acertou");
+            
+        }else if(resposta > numeroSorteado){
+            escrever("O número sorteado é menor");
+            tentativas--;
+        }else if(resposta < numeroSorteado){
+            escrever("O número sorteado é maior");
+            tentativas--
+        }
+    
+    if(resposta != numeroSorteado && tentativas == 0){
+        escrever("Suas tentativas acabaram. O número sorteado era "+ numeroSorteado);
+    }
+});
+
+recarregar.addEventListener("click", function(){
+    document.location.reload();
+});
